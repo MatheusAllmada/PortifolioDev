a9567fdaf655000a1eb4797be99bb8a44d3d5849
matheus almada <contatomatheusbarreto@hotmail.com>
2023-04-29 21:55:50 -0300

    mensagem

--- src/Projetos/index.jsx
+++ src/Projetos/index.jsx
@@ -6,27 +6,42 @@ export const Projetos = () => {
       <h2>Projetos:</h2>
       <Projetoscard>
         <Cards>
-          <img src="/assets/imgs-projetos/flexblog.png" alt="" />
+          <a target="blank" href="https://projetoflexblog.vercel.app/">
+            <img src="/assets/imgs-projetos/flexblog.png" alt="" />
+          </a>
         </Cards>
         <Cards>
+          <a target="blank" href="https://lobo-landing-page-master.vercel.app/">
           <img src="/assets/imgs-projetos/loboinfo.png" alt="" />
+          </a>
+          
         </Cards>
         <Cards>
-          <img src="/assets/imgs-projetos/bikemaster.png" alt="" />
+          <a target="blank" href="https://bicicleta-landing-page-1.vercel.app/">
+            <img src="/assets/imgs-projetos/bikemaster.png" alt="" /></a>
+          
         </Cards>
         <Cards>
-          <img src="/assets/imgs-projetos/teste.png" alt="" />
+          <a target="blank" href="https://lobogrid.vercel.app/">
+            <img src="/assets/imgs-projetos/teste.png" alt="" /></a>
+          
         </Cards>
         <Cards>
+          <a target="blank" href="https://bicileta-html-css.vercel.app/">
           <img src="/assets/imgs-projetos/bike.png" alt="" />
+          </a>
+          
         </Cards>
         <Cards>
+          <a target="blank" href=""></a>
           <img src="/assets/imgs-projetos/animaisfantasticos.png" alt="" />
         </Cards>
         <Cards>
+          <a target="blank" href=""></a>
           <img src="/assets/imgs-projetos/lojacarros.png" alt="" />
         </Cards>
         <Cards>
+          <a target="blank" href=""></a>
           <img src="/assets/imgs-projetos/axiosblog.png" alt="" />
         </Cards>
       </Projetoscard>
